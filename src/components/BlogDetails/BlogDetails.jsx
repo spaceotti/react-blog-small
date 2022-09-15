e4fc384a1d529e21557bb2dbcc5afd8f88183cf8
spaceotti <spaceotti1981@gmail.com>
2022-09-15 09:52:44 +0200

    Small change in components

--- src/components/BlogDetails/BlogDetails.jsx
+++ src/components/BlogDetails/BlogDetails.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHistory, useParams } from "react-router-dom";
-import useFetch from "../hooks/useFetch";
+import useFetch from "../../hooks/useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
